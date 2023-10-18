118ddd4f7bc07f6c2983ff637c1e4e22bcd10dfc
amar-ti2021 <muha21095ti@student.nurulfikri.ac.id>
2023-10-18 22:16:43 +0700

    fix: missing hooks

--- src/components/pro/Navbar/Navbar.jsx
+++ src/components/pro/Navbar/Navbar.jsx
@@ -1,7 +1,8 @@
 import React from "react";
-import { Link } from "react-router-dom";
+import { Link, useLocation } from "react-router-dom";
 import style from "./Navbar.module.css";
 const Navbar = () => {
+  const location = useLocation();
   return (
     <nav>
       <Link to={"/pro"}>
