a36f0142ad682c8626bd2cdda19c65642dbf3684
amar-ti2021 <muha21095ti@student.nurulfikri.ac.id>
2023-10-19 09:44:32 +0700

    fix: merge error

--- src/App.jsx
+++ src/App.jsx
@@ -3,34 +3,7 @@ import { Routes, Route, RouterProvider } from "react-router-dom";
 import router from "./lib/router/router";
 
 function App() {
-<<<<<<< HEAD
-  return (
-    <div>
-      <NavbarComponent />
-
-      <Routes>
-        <Route path="/" Component={HomePage} />
-        
-        <Route path="/morearticle" Component={MoreArticle} />
-        <Route path="/moredesign" Component={MoreDesign} />
-        <Route path="/morepro" Component={MorePro} />
-
-        <Route path="/readarticle" Component={ReadArticle} />
-        <Route path="/readdesign" Component={ReadDesign} />
-        <Route path="/readpro" Component={ReadPro} />
-        <Route path="/pro" Component={ProNetwork} />
-
-        <Route path="/login" Component={LoginPage} />
-        <Route path="/register" Component={RegisterPage} />
-        <Route path="/sesudah-login" Component={SesudahLoginPage} />
-      </Routes>
-
-      <FooterComponent />
-    </div>
-  );
-=======
   return <RouterProvider router={router} />;
->>>>>>> 069a0974c02eea25a322c9429e07efc2c48b8056
 }
 
 export default App;
--- src/pages/LoginPage.jsx
+++ src/pages/LoginPage.jsx
@@ -3,44 +3,21 @@ import { Container, Row, Col } from "react-bootstrap";
 import { NavLink } from "react-router-dom";
 
 const LoginPage = () => {
-return (
+  return (
     <div className="login-page">
-        <Container>
-            <Row>
-                <Col>
-                <section className="login-section" id="login">
-                    <div className="login-container grid">
-                        <div className="login-card rounded-5">
-                            <div className="login-description">
-                                <h2><b>Sign In</b></h2>
-                            </div>
-                
-                            <form className="login-form">
-                                <div className="login-box">
-                                    <label for="email" className="login-label-email fw-bold">Email</label>
-                                    <input type="email" className="login-input" placeholder="Enter Your Email" />
-                                </div>
-                                <div class="login-box">
-                                    <label for="password" className="login-label-password fw-bold">Password</label>
-                                    <input type="password" className="login-input" placeholder="Enter Your Password" />
-                                </div>
-                                <NavLink to="/sesudah-login" className="login-button rounded-5 fw-bold">Sign In</NavLink>
-                                <p className="pt-3">Don't Have an Account? <NavLink to="/register" className="login-signup">
-                                    Sign up
-                                </NavLink>
-                                </p>
-                            </form>
-                        </div>
-                    </div>
-                </section>
-                </Col>
-            </Row>
-        </Container>
-    </div>
-  )
-}
+      <Container>
+        <Row>
+          <Col>
+            <section className="login-section" id="login">
+              <div className="login-container grid">
+                <div className="login-card rounded-5">
+                  <div className="login-description">
+                    <h2>
+                      <b>Sign In</b>
+                    </h2>
+                  </div>
 
-                  <form action="" className="login-form">
+                  <form className="login-form">
                     <div className="login-box">
                       <label for="email" className="login-label-email fw-bold">
                         Email
@@ -48,8 +25,6 @@ return (
                       <input
                         type="email"
                         className="login-input"
-                        id="email"
-                        name="user-email"
                         placeholder="Enter Your Email"
                       />
                     </div>
@@ -63,17 +38,15 @@ return (
                       <input
                         type="password"
                         className="login-input"
-                        id="password"
-                        name="user-password"
                         placeholder="Enter Your Password"
                       />
                     </div>
-                    <button
+                    <NavLink
+                      to="/sesudah-login"
                       className="login-button rounded-5 fw-bold"
-                      onClick={() => navigate("/sesudah-login")}
                     >
-                      Sign in
-                    </button>
+                      Sign In
+                    </NavLink>
                     <p className="pt-3">
                       Don't Have an Account?{" "}
                       <NavLink to="/register" className="login-signup">
